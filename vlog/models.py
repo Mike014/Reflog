7fd8fa57937d3f5c789c060cb4e3d0a95b687a31
Mike014 <mikgrimaldi7@gmail.com>
2025-07-05 10:43:35 +0200

    Added second video

--- vlog/models.py
+++ vlog/models.py
@@ -14,3 +14,18 @@ class VlogPost(models.Model):
     def __str__(self):
         return self.title
 
+
+
+# from django.db import models
+
+# class VlogPost(models.Model):
+#     title = models.CharField(max_length=200)
+#     text_reflection = models.TextField(blank=True)
+#     video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+#     video_url = models.URLField(blank=True, null=True)
+#     audio_note = models.FileField(upload_to='audio/', blank=True, null=True)
+#     tags = models.CharField(max_length=200, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.title
